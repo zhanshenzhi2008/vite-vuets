77564f6d884a3b9ea3bba2eb294f105bdce77b3f
liujun <jun.liu_southchina@convertlab.com>
2024-07-08 18:09:20 +0800

    feat @types/node 模块在我们使用 node 方法（比如 path.resolve）时提供 ts 类型声明，否则编辑器会报错，虽然不影响代码运行，但是会有红线很影响心情。

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import autoprefixer from 'autoprefixer'
-import VitePluginWindicss from "vite-plugin-windicss";
+import VitePluginWindicss from "vite-plugin-windicss"
+import {resolve} from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [
-        autoprefixer({
-          overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
-        })
-      ]
-    }
-  },
-  plugins: [vue(), VitePluginWindicss()],
+    css: {
+        postcss: {
+            plugins: [
+                autoprefixer({
+                    overrideBrowserslist: ['Chrome > 40', 'ff > 31', 'ie 11'],
+                })
+            ]
+        }
+    },
+    resolve: {
+        alias: [
+            {
+                find: '@',
+                replacement: resolve(__dirname, 'src')
+            }
+        ],
+    },
+    plugins: [vue(), VitePluginWindicss()],
 })
