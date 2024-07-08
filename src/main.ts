7cf85421197762b0255e872b7223dfb903a2fc04
liujun <jun.liu_southchina@convertlab.com>
2024-07-08 17:48:49 +0800

    feat 使用 vue-router 且登录页面

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import {createApp} from 'vue'
 import {createPinia} from 'pinia'
 import './style.css'
 import App from './App.vue'
+import router from './router/index'
 import './common.scss'
 import 'virtual:windi.css'
 import formCreate from '@form-create/element-ui'
@@ -9,4 +10,5 @@ import formCreate from '@form-create/element-ui'
 const app = createApp(App)
 app.use(createPinia())
 app.use(formCreate)
+app.use(router)
 app.mount('#app');
