fa726fa69ca6b76c23ea611db2c34ce45dee26b8
liujun <jun.liu_southchina@convertlab.com>
2024-07-08 17:24:34 +0800

    feat 增加 windi.config.ts 配置文件

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
+  <div class="text-pink-500">text windicss</div>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
