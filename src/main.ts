b22fbccf5e189f37e0fbf80bf0dc5e6265264323
liujun <jun.liu_southchina@convertlab.com>
2024-07-08 17:22:11 +0800

    feat 安装 vite-plugin-windicss 和 windicss

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import './common.scss'
+import 'virtual:windi.css'
+
 createApp(App).mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import autoprefixer from 'autoprefixer'
-
+import VitePluginWindicss from "vite-plugin-windicss";
 // https://vitejs.dev/config/
 export default defineConfig({
   css: {
@@ -13,5 +13,5 @@ export default defineConfig({
       ]
     }
   },
-  plugins: [vue()],
+  plugins: [vue(), VitePluginWindicss()],
 })
