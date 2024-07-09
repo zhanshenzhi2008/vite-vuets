a6cbd088658c9e158a11acaa41d01acd833cac34
liujun <jun.liu_southchina@convertlab.com>
2024-07-09 14:43:43 +0800

    feat 命名

--- src/components/DemoLanguage.vue
+++ src/components/DemoLanguage.vue
@@ -4,6 +4,7 @@
 <script>
 import En from "@form-create/designer/locale/en.js";
 import Cn from "@form-create/designer/locale/zh-cn.js";
+import Jp from "@form-create/designer/locale/jp.js";
 export default {
   data(){
     return {
--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -1,12 +1,14 @@
 <template>
   <div @click="toLogin"> 返回</div>
   <Demo-form></Demo-form>
+  <DemoFcDesigner/>
   <router-view> </router-view>
 </template>
 <script lang="ts" setup>
 
 import { useRoute, useRouter } from 'vue-router'
 import DemoForm from '@/components/DemoForm.vue';
+import DemoFcDesigner from "@/components/DemoFcDesigner.vue";
 
 const router = useRouter()
 function toLogin() {
