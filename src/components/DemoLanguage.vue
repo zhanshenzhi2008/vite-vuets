0e60945db680010efd5e595ed970f1a7f7558027
liujun <jun.liu_southchina@convertlab.com>
2024-07-09 14:34:42 +0800

    feat 添加语言包

---
+++ src/components/DemoLanguage.vue
@@ -0,0 +1,14 @@
+<template>
+  <fc-designer :locale="locale" />
+</template>
+<script>
+import En from "@form-create/designer/locale/en.js";
+import Cn from "@form-create/designer/locale/zh-cn.js";
+export default {
+  data(){
+    return {
+      locale: En,
+    }
+  }
+}
+</script>
